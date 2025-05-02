93c6ab924d5c9469bade888f13807ccf576202e8
xfiser1 <34626149+vikimiki11@users.noreply.github.com>
2025-05-02 10:52:17 +0200

    reformating

--- example.c
+++ example.c
@@ -8,22 +8,21 @@
  * 4. Print the IV and the plaintext
  */
 int main(void) {
-    uint8_t message[48];
-    uint8_t iv[16];
-    uint8_t ciphertext[32];
-    uint8_t plaintext[32];
-  
-    // iv || ciphertext
-    recv_message(message);
-    
-    memcpy(iv, message, 16);
-    memcpy(ciphertext, message + 16, 48);
+	uint8_t message[48];
+	uint8_t iv[16];
+	uint8_t ciphertext[32];
+	uint8_t plaintext[32];
 
-    decrypt_message(iv, ciphertext, plaintext);
+	// iv || ciphertext
+	recv_message(message);
 
-    print_iv(iv);
-    print_plaintext(plaintext);
+	memcpy(iv, message, 16);
+	memcpy(ciphertext, message + 16, 48);
+
+	decrypt_message(iv, ciphertext, plaintext);
+
+	print_iv(iv);
+	print_plaintext(plaintext);
 
 }
-
 
